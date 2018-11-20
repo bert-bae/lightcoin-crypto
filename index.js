18df982249f7e313e26cdef6b3ad34f3befe4e3f
Elbert <elbert.bae@gmail.com>
2018-11-20 21:33:39 +0000

    working with OOP

--- index.js
+++ index.js
@@ -1,15 +1,60 @@
 let balance = 500.00;
 
-class Withdrawal {
+class account {
+  constructor(name) {
+    this.name = name;
+    this.transactions = [];
+    this.log = [];
+    this.balance = 0;
+  }
+  getBalance() {
+    return this.transactions.reduce((a, b) => { return a + b; });
+  }
+}
 
-  constructor(amount) {
+class transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    let today = new Date().toDateString();
+    let response = `on ${today}: ${this.value}`;
+    if (this.value > 0) {
+      if (this.account.balance) {
+        this.account.transactions.push(this.value);
+        this.account.balance += this.value;
+        this.account.log.push(`Deposit confirmed ${response}. Account balance ${this.account.balance}`);
+      } else {
+        this.account.transactions.push(this.value);
+        this.account.balance = this.value;
+        this.account.log.push(`Deposit confirmed ${response}. Account balance ${this.account.balance}`);
+      }
+    } else {
+      if ((this.account.balace - this.value) > 0) {
+        this.account.transactions.push(this.value);
+        this.account.balance += this.value;
+        this.account.log.push(`Withdrawal confirmed ${response}. Account balance ${this.account.balance}`);
+      } else {
+        this.account.log.push(`Withdrawal denied ${response}. Account balance ${this.account.balance}`);
+      }
+    }
   }
+}
 
+class withdrawal extends transaction {
+
+  get value() {
+    return -this.amount;
+  }
+}
+
+class deposit extends transaction {
+
+  get value() {
+    return this.amount;
+  }
 }
 
 
@@ -17,13 +62,19 @@ class Withdrawal {
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const bertAccount = new account('bertAccount');
 
-t1 = new Withdrawal(50.25);
+let t1 = new deposit(150, bertAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
-
-t2 = new Withdrawal(9.99);
+let t2 = new withdrawal(500, bertAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
+let t3 = new deposit(1200, bertAccount);
+t3.commit();
+let t4 = new withdrawal(2000, bertAccount);
+t4.commit();
+let t5 = new deposit(1.50, bertAccount);
+t5.commit();
+let t6 = new withdrawal(700, bertAccount);
+t6.commit();
 
-console.log('Balance:', balance);
+console.log(bertAccount.log);
